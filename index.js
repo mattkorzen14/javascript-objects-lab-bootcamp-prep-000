456bcdca4d78f76f4399f57faf362711ad00f695
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-14 05:40:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object.key = value;
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  
+  return object.key = value;
 }
