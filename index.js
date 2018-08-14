d09d95e41e9527e4043ab1fa3240e5890d2d261f
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-14 05:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey (object, key) {
+  
+}
+function destructivelyDeleteFromObjectByKey (object, key) {
+  
+}
