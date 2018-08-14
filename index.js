5a4aa46dbe8a9b1d80ec4323427559354f0badae
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-14 05:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = new Object();
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.key = value;
+ object[key] = value;
+ return object;
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object[key] = value;
